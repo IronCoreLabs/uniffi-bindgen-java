31ed84a72b04b6753b292d4c2b7c578db502e75c
Murph Murphy <murph@clurictec.com>
2024-05-22 17:12:09 -0600

    Compiling, following test failures

--- src/templates/OptionalTemplate.java
+++ src/templates/OptionalTemplate.java
@@ -3,19 +3,20 @@ package {{ config.package_name() }};
 
 import java.nio.ByteBuffer;
 
+// public class TestForOptionals {}
 
 {#- 
   Swift, Python, and Go bindings all use nullable/nilable optionals instead of `Optional` types where they exist.
   Kotlin and C# bindings use their type system to directly express nullability with `?`.
   Java has an `Optional` type that some FP Java folks use, but we'll lean on the more straightforward Java way here
   and have it be invisibly nullable, because that's the normal Java way.
--#}
+#}
 public enum {{ ffi_converter_name }} implements FfiConverterRustBuffer<{{ inner_type_name }}> {
   INSTANCE;
 
   @Override
   public {{ inner_type_name }} read(ByteBuffer buf) {
-    if (buf.get().toInt() == 0) {
+    if (buf.get() == (byte)0) {
       return null;
     }
     return {{ inner_type|read_fn }}(buf);
@@ -24,18 +25,18 @@ public enum {{ ffi_converter_name }} implements FfiConverterRustBuffer<{{ inner_
   @Override
   public long allocationSize({{ inner_type_name }} value) {
     if (value == null) {
-      return 1UL;
+      return 1L;
     } else {
-      return 1UL + {{ inner_type|allocation_size_fn }}(value);
+      return 1L + {{ inner_type|allocation_size_fn }}(value);
     }
   }
 
   @Override
   public void write({{ inner_type_name }} value, ByteBuffer buf) {
     if (value == null) {
-      buf.put(0);
+      buf.put((byte)0);
     } else {
-      buf.put(1);
+      buf.put((byte)1);
       {{ inner_type|write_fn }}(value, buf);
     }
   }
--- tests/scripts/TestRondpoint.java
+++ tests/scripts/TestRondpoint.java
@@ -5,20 +5,20 @@ import uniffi.rondpoint.*;
 
 public class TestRondpoint {
   public static void main(String[] args) throws Exception {
-    Dictionarre dico = new Dictionnaire(Enumeration.DEUX, true, 0, 123456789);
-    Dictionarre copyDico = Rondpoint.copyDico(dico);
+    Dictionnaire dico = new Dictionnaire(Enumeration.DEUX, true, (byte)0, 123456789L);
+    Dictionnaire copyDico = Rondpoint.copieDictionnaire(dico);
     assert dico == copyDico;
     
     assert Rondpoint.copieEnumeration(Enumeration.DEUX) == Enumeration.DEUX;
-    assert Rondpoint.copieEnumeration(List.of(Enumeration.UN, Enumeration.DEUX) == List.of(Enumeration.UN, Enumeration.DEUX));
+    assert Rondpoint.copieEnumerations(List.of(Enumeration.UN, Enumeration.DEUX)) == List.of(Enumeration.UN, Enumeration.DEUX);
     assert Rondpoint.copieCarte(Map.ofEntries(
-      Map.entry("0", EnumerationAvecDonnees.Zero),
-      Map.entry("1", EnumerationAvecDonnees.Un(1)),
-      Map.entry("2", EnumerationAvecDonnees.Deux(2, "deux"))
+      Map.entry("0", new EnumerationAvecDonnees.Zero()),
+      Map.entry("1", new EnumerationAvecDonnees.Un(1)),
+      Map.entry("2", new EnumerationAvecDonnees.Deux(2, "deux"))
     )) == Map.ofEntries(
-      Map.entry("0", EnumerationAvecDonnees.Zero),
-      Map.entry("1", EnumerationAvecDonnees.Un(1)),
-      Map.entry("2", EnumerationAvecDonnees.Deux(2, "deux"))
+      Map.entry("0", new EnumerationAvecDonnees.Zero()),
+      Map.entry("1", new EnumerationAvecDonnees.Un(1)),
+      Map.entry("2", new EnumerationAvecDonnees.Deux(2, "deux"))
     );
   }
 }
