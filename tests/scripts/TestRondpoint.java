42ae4f3f258fcd5828b6d6b7ffbcbb3aef5902b5
Murph Murphy <murph@clurictec.com>
2024-05-22 17:27:23 -0600

    Working minimal rondpoint test!

--- tests/scripts/TestRondpoint.java
+++ tests/scripts/TestRondpoint.java
@@ -7,18 +7,18 @@ public class TestRondpoint {
   public static void main(String[] args) throws Exception {
     Dictionnaire dico = new Dictionnaire(Enumeration.DEUX, true, (byte)0, 123456789L);
     Dictionnaire copyDico = Rondpoint.copieDictionnaire(dico);
-    assert dico == copyDico;
+    assert dico.equals(copyDico);
     
-    assert Rondpoint.copieEnumeration(Enumeration.DEUX) == Enumeration.DEUX;
-    assert Rondpoint.copieEnumerations(List.of(Enumeration.UN, Enumeration.DEUX)) == List.of(Enumeration.UN, Enumeration.DEUX);
+    assert Rondpoint.copieEnumeration(Enumeration.DEUX).equals(Enumeration.DEUX);
+    assert Rondpoint.copieEnumerations(List.of(Enumeration.UN, Enumeration.DEUX)).equals(List.of(Enumeration.UN, Enumeration.DEUX));
     assert Rondpoint.copieCarte(Map.ofEntries(
       Map.entry("0", new EnumerationAvecDonnees.Zero()),
       Map.entry("1", new EnumerationAvecDonnees.Un(1)),
       Map.entry("2", new EnumerationAvecDonnees.Deux(2, "deux"))
-    )) == Map.ofEntries(
+    )).equals(Map.<String, EnumerationAvecDonnees>ofEntries(
       Map.entry("0", new EnumerationAvecDonnees.Zero()),
       Map.entry("1", new EnumerationAvecDonnees.Un(1)),
-      Map.entry("2", new EnumerationAvecDonnees.Deux(2, "deux"))
+      Map.entry("2", new EnumerationAvecDonnees.Deux(2, "deux")))
     );
   }
 }
