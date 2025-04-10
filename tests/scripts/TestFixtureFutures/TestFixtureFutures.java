1f57c1405ea027d6ddb14f53fd55d7e72bac429a
Murph Murphy <murph@clurictec.com>
2025-04-10 17:39:54 -0600

    Remove dead import

--- tests/scripts/TestFixtureFutures/TestFixtureFutures.java
+++ tests/scripts/TestFixtureFutures/TestFixtureFutures.java
@@ -5,7 +5,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class TestFixtureFutures {
